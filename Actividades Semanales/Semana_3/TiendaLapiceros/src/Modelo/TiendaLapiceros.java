f5f5b5e553d8f9c44746a083f23b542eb6dc01e5
Jiho <t00050a@ms.upla.edu.pe>
2025-10-04 23:04:53 -0500

    Add files via upload

---
+++ Actividades Semanales/Semana_3/TiendaLapiceros/src/Modelo/TiendaLapiceros.java
@@ -0,0 +1,34 @@
+package Modelo;
+
+public class TiendaLapiceros {
+    private double precioDocena;
+    private int cantidadDocenas;
+
+    public TiendaLapiceros(double precioDocena, int cantidadDocenas) {
+        this.precioDocena = precioDocena;
+        this.cantidadDocenas = cantidadDocenas;
+    }
+
+    public double calcularImporteCompra() {
+        return precioDocena * cantidadDocenas;
+    }
+
+    public double calcularDescuento() {
+        double importe = calcularImporteCompra();
+        double porcDescuento = (cantidadDocenas >= 10) ? 0.20 : 0.10;
+        return importe * porcDescuento;
+    }
+
+    public double calcularImportePagar() {
+        return calcularImporteCompra() - calcularDescuento();
+    }
+
+    public int calcularLapiceros() {
+        double importePagar = calcularImportePagar();
+        if (importePagar >= 200) {
+            return 2 * cantidadDocenas;
+        } else {
+            return 0;
+        }
+    }
+}
